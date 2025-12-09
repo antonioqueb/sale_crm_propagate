6f6cd553492ee862d069b0ade111d46c9002eb0c
Antonio Queb <cardstillo@gmail.com>
2025-12-08 20:33:08 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'CRM to Sale Propagate',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Sales/CRM',
     'summary': 'Propaga campos y líneas de residuos de CRM a cotizaciones',
     'author': 'Alphaqueb Consulting',
